bc26f53df11a923af492672c6b195145f836101f
LAPTOP-VK5FFT7Q\pooh6 <johnny062995@gmail.com>
2023-04-15 01:33:20 +0800

    新增"天文"目錄、增加index button與其對應js檔

---
+++ javascript_00.js
@@ -0,0 +1,9 @@
+
+function F(){
+    window.location.href="https://yangchenghan629.github.io/chy-web/astronomy_index.html";
+}
+window.onload=function(){
+    const element=document.getElementById("btn");
+    element.addEventListener("click",F);
+
+}
